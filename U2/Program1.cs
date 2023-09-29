0fb8f076e2082b5f3039f26633e93e6a9f6cc460
TheShadowMexican05 <146400559+TheShadowMexican05@users.noreply.github.com>
2023-09-29 15:09:22 -0600

    P_1 y  P_2

---
+++ U2/Program1.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine es parea escribir texto este se escribe rentre comillas dobles*/
+
+using System;
+
+class Hola
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingresa tu nombre: ");
+        string Nombre;
+        Nombre = Console.ReadLine();
+        Console.WriteLine("Hola " + Nombre);
+
+        Console.WriteLine("Ingresa tu edad: ");
+        int Edad;
+        Edad = Int32.Parse(Console.ReadLine());
+        
+        if (Edad < 18)
+        {
+            Console.WriteLine ("Eres menor de edad");
+        }else if (Edad <= 85)
+        {
+            Console.WriteLine("Eres mayor de edad");
+        }else
+        {
+            Console.WriteLine("Eres una momia o que pdo");
+        }
+    }
+}
---
+++ U2/Program2.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+namespace P_2
+{
+    class Suma
+    {
+        static void Main()
+        {
+            int A = 16, B = 25, Suma;
+            Suma = A + B;
+            Console.WriteLine(Suma);
+
+            /*Console.WriteLine("Ingresa el primer numero: ");
+            int Num1;
+            Num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo numero: ");
+            int Num2;
+            Num2 = Int32.Parse(Console.ReadLine());
+            int Suma;
+            Suma = Num1 + Num2;
+            Console.WriteLine("La suma es igual a: " + Suma);*/
+
+        }
+    }
+}
