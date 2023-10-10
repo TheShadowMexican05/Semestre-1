0d349ba85a54d41c4eb8cc40cadd0a1eb8d1139e
TheShadowMexican05 <146400559+TheShadowMexican05@users.noreply.github.com>
2023-10-09 19:45:11 -0600

    Java

--- JavaU2/P_7.java
+++ JavaU2/P_7.java
@@ -14,21 +14,3 @@ public class Main
 		System.out.println(gc+"°C son iguales a: "+gf+"°F.");
 	}
 }
-
-5- Metros a pies.
-
-import java.util.Scanner;
-
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner M = new Scanner(System.in);
-		double f;
-		
-		System.out.println("Ingresa los metros a convertir: ");
-		double m = M.nextDouble();
-		f = m * 3.281;
-		System.out.println(m+"m son iguales a: "+f+"ft");
-	}
-}
-
