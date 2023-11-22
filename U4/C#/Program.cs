4a901114a6bb69457c0f69c715570a23b67fa1fa
TheShadowMexican05 <146400559+TheShadowMexican05@users.noreply.github.com>
2023-11-21 19:55:22 -0600

    Add files via upload

---
+++ U4/C#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("¿Cuántos nombres deseas ingresar?: ");
+        int CN = int.Parse(Console.ReadLine());
+
+        var N = new string[CN];
+        var A = new string[CN];
+
+        for (int i = 0; i < CN; i++)
+        {
+            Console.Write($"Ingrese el nombre {i + 1}: ");
+            N[i] = Console.ReadLine();
+
+            Console.Write($"Ingrese el apellido {i + 1}: ");
+            A[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Nombres completos:");
+        for (int i = 0; i < CN; i++)
+        {
+            Console.WriteLine($"{N[i]} {A[i]}");
+        }
+    }
+}
