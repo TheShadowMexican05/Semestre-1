3256f9ef69fad77cb9902bb0c3185a0e718e9afc
TheShadowMexican05 <146400559+TheShadowMexican05@users.noreply.github.com>
2023-11-21 19:39:30 -0600

    Add files via upload

---
+++ U4/C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa los elementos del arreglo separados por espacios: ");
+        int[] N = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        int[] pares = new int[N.Length];
+        int[] impares = new int[N.Length];
+        int i = 0, j = 0;
+
+        foreach (var n in N)
+        {
+            if (n % 2 == 0)
+            {
+                pares[i++] = n;
+            }
+            else
+            {
+                impares[j++] = n;
+            }
+        }
+
+        Array.Resize(ref pares, i);
+        Array.Resize(ref impares, j);
+
+        Console.WriteLine($"Elementos pares: {string.Join(", ", pares)}");
+        Console.WriteLine($"Elementos impares: {string.Join(", ", impares)}");
+    }
+}
